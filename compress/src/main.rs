e380291071700023c84c103489309fd48dd32d08
aaraam <aaramblkice@gmail.com>
2024-02-12 16:38:23 +0530

    feat: initial

---
+++ compress/src/main.rs
@@ -0,0 +1,16 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main() {
+    if args().len() != 3 {
+        epintln("Usage: `source` `target`");
+        return;
+    }
+}
